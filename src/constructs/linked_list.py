da4b81701f3e0c219779d59a0f6908ae6fea048c
mahbod <m.ghadirisani2013@gmail.com>
2022-12-27 15:45:07 +0330

    refactor

--- src/constructs/linked_list.py
+++ src/constructs/linked_list.py
@@ -4,28 +4,14 @@ class Node:
         self.data = data
         self.next = next
     
-def generate_linked_list(n: int, create_circle, create_text, frame_data: dict) -> Node:
+def generate_linked_list(n: int) -> Node:
     #root is just a place holder
     root = Node(0)
     current = root
-    mid_position = frame_data["mid_position"]
-    r_middle = frame_data["r_middle"]
-    theta = frame_data["theta"]
     for i in range(1,n+1):
-        x = r_middle * sin(radians((i-1) * (theta) + 180)) + mid_position[0]
-        y = r_middle * cos(radians((i-1) * (theta) + 180)) + mid_position[1]
-        component = create_circle(x,y, fill="green")
-        num = create_text(x,y,text=i,fill="black")
-        data = {
-            "num": num,
-            "component": component
-        }
-        new_node = Node(data)
+        new_node = Node(i)
         current.next = new_node
         current = current.next
     current.next = root.next
     current = current.next
     return current
-
-
-
--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from constructs.linked_list import generate_linked_list, Node
-from math   import sin, radians
+from math   import sin, cos, radians
 #adding a custom method to Canvas class
 def _create_circle(self, x, y, r = 9, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
@@ -41,48 +41,61 @@ class GUI:
         #set window
         window = tk.Toplevel(self.root)
         self.window = window
-
-        theta = 360/n
-        r_middle = float(12 / sin(radians(theta/2))) if float(10 / sin(radians(theta/2)))>50 else 50
-        frame_width = int(2*r_middle + 200)
-        frame_height = frame_width 
+        self.n = n
+        self.drawLinkedList()
         #set size
-        window.geometry(f"{frame_width}x{frame_height}")
+        window.geometry(f"{self.frame_width}x{self.frame_height}")
         #window.minsize(frame_width , frame_height)
         #window.maxsize(frame_width , frame_height)
-        self.canvas = tk.Canvas(window, width=frame_width, height=frame_height-20)
-        canvas = self.canvas
-        canvas.pack(expand=True, fill=tk.BOTH)
-        mid_position = (frame_width/2,frame_height/2)
-        frame_data = {
-            "mid_position": mid_position,
-            "r_middle": r_middle,
-            "theta": theta
-        }
-        self.current = generate_linked_list(n, canvas.create_circle, canvas.create_text, frame_data)
-        canvas.itemconfig(self.current.data["component"], fill="blue")
         tk.Button(window,
             text= "Kill Next",
             command= self.kill_next
             ).pack()
 
-    def kill_next(self)->Node:
-        current = self.current
+    def kill_next(self, k = 1)->Node:
+        current = self.head
         canvas = self.canvas
-        canvas.itemconfig(self.current.data["component"], fill="green")
-        canvas.itemconfig(current.next.data["component"], fill="red")
+        canvas.itemconfig(self.getCircleId(current.data), fill="green")
+        canvas.itemconfig(self.getCircleId(current.next.data), fill="red")
         current.next = current.next.next
-        self.current = current.next
-        canvas.itemconfig(self.current.data["component"], fill="blue")
+        self.head = current.next
+        canvas.itemconfig(self.getCircleId(self.head.data), fill="blue")
         if(current.next == current.next.next):
             popup = tk.Toplevel(self.window)
             popup.geometry("300x50")
             popup.minsize(300,50)
             popup.maxsize(300,50)
-            label = tk.Label(popup, text = f"Number {int(self.current.data['num']/2)} survives!")
+            label = tk.Label(popup, text = f"Number {int(self.head.data)} survives!")
             close_button = tk.Button(popup, text="Ok", command=self.close_window)
             label.pack()
             close_button.pack()
 
     def close_window(self):
         self.window.destroy()
+
+    def drawLinkedList(self):
+        n = self.n
+        self.theta = 360/n
+        self.r_middle = float(12 / sin(radians(self.theta/2))) if float(10 / sin(radians(self.theta/2)))>50 else 50
+        self.frame_width = int(2*self.r_middle + 200)
+        self.frame_height = self.frame_width 
+        self.canvas = tk.Canvas(self.window, width=self.frame_width, height=self.frame_height-20)
+        canvas = self.canvas
+        canvas.pack(expand=True, fill=tk.BOTH)
+        self.mid_position = (self.frame_width/2 ,self.frame_height/2)
+        self.head = generate_linked_list(n)
+        self.drawNode(self.head)
+        current = self.head.next
+        while current != self.head:
+            self.drawNode(current)
+            current = current.next
+        canvas.itemconfig(self.head.data, fill="blue")
+
+    def drawNode(self, node: Node):
+        x = self.r_middle * sin(radians((node.data-1) * (self.theta) + 180)) + self.mid_position[0]
+        y = self.r_middle * cos(radians((node.data-1) * (self.theta) + 180)) + self.mid_position[1]
+        self.canvas.create_circle(x,y, fill="green")
+        self.canvas.create_text(x,y,text=node.data,fill="black")
+
+    def getCircleId(self, number: int) -> int:
+        return 2*number-1
