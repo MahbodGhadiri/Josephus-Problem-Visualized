230d9682dc2e85e954715d23e74c6478f0d7aabd
mahbod <m.ghadirisani2013@gmail.com>
2022-12-31 08:10:50 +0330

    Better error messages + keboard events + Scrollbar

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from tkinter import messagebox
 from constructs.linked_list import generate_linked_list, Node
 from math   import sin, cos, radians
+
+_r = 12
 #adding a custom method to Canvas class
-def _create_circle(self, x, y, r = 9, **kwargs):
+def _create_circle(self, x, y, r = _r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 tk.Canvas.create_circle = _create_circle
 
@@ -25,6 +28,10 @@ class GUI:
         #add button
         self.button = tk.Button(text="Visualize", command=self.visualize)
         self.button.pack()
+        #add state
+        self.hasDrawnlinkedList = False
+        self.isKilling = False
+
         root.mainloop()
 
 
@@ -32,27 +39,34 @@ class GUI:
         try:
             n = int(self.entry.get())
         except:
+            self.showInvalidInputDialog(self.entry.get())
             self.entry.delete(0, tk.END)
-            #self.entry.insert(0,"")
             return
         if n<=0:
+            self.showInputTooSmallDialog(n)
             self.entry.delete(0, tk.END)
             return
+        if n> 500:
+            self.showInputTooLargeDialog(n)
+            self.entry.delete(0, tk.END)
+            return
+        self.button["state"] = "disable"
         #set window
         window = tk.Toplevel(self.root)
         self.window = window
+        # bind keyboard event
+        window.bind("<space>", self.kill_next)
+        window.bind("<Return>", self.kill_all)
+
         self.n = n
         self.drawLinkedList()
         #set size
         window.geometry(f"{self.frame_width}x{self.frame_height}")
-        #window.minsize(frame_width , frame_height)
-        #window.maxsize(frame_width , frame_height)
-        tk.Button(window,
-            text= "Kill Next",
-            command= self.kill_next
-            ).pack()
 
-    def kill_next(self, k = 1)->Node:
+    def kill_next(self, event)->Node:
+        if(self.isKilling):
+            return
+        self.isKilling = True
         current = self.head
         canvas = self.canvas
         canvas.itemconfig(self.getCircleId(current.data), fill="green")
@@ -61,34 +75,58 @@ class GUI:
         self.head = current.next
         canvas.itemconfig(self.getCircleId(self.head.data), fill="blue")
         if(current.next == current.next.next):
-            popup = tk.Toplevel(self.window)
-            popup.geometry("300x50")
-            popup.minsize(300,50)
-            popup.maxsize(300,50)
-            label = tk.Label(popup, text = f"Number {int(self.head.data)} survives!")
-            close_button = tk.Button(popup, text="Ok", command=self.close_window)
-            label.pack()
-            close_button.pack()
+            self.showSurvivor()
+        self.isKilling = False
+
+    def kill_all(self, event):
+        pass
 
     def close_window(self):
         self.window.destroy()
 
     def drawLinkedList(self):
+
         n = self.n
         self.theta = 360/n
-        self.r_middle = float(12 / sin(radians(self.theta/2))) if float(10 / sin(radians(self.theta/2)))>50 else 50
+        '''
+            r = sin(theta/2) * R => R = r/sin(theta/2)  
+            The above formula is used to calculate r_middle value (r_middle is R in above formula)
+            r_middle is the radius of the invisible circle, on which the center of the small circles are draw.
+            theta is the angle between to circles
+            r is the radius of small circles
+        '''
+        self.r_middle = float((_r+1) / sin(radians(self.theta/2))) if float((_r+1) / sin(radians(self.theta/2)))>50 else 50
         self.frame_width = int(2*self.r_middle + 200)
         self.frame_height = self.frame_width 
-        self.canvas = tk.Canvas(self.window, width=self.frame_width, height=self.frame_height-20)
+        #add canvas
+        self.canvas = tk.Canvas(
+            self.window,
+            width=self.frame_width-10,
+            height=self.frame_height-20,
+            scrollregion=(0,0,self.frame_width, self.frame_height)
+        )
         canvas = self.canvas
+        # add horizontal scrollbar
+        hbar=tk.Scrollbar(self.window, orient=tk.HORIZONTAL)
+        hbar.pack(side=tk.TOP,fill=tk.X)
+        hbar.config(command=canvas.xview)
+        # add verticall scrollbar
+        vbar=tk.Scrollbar(self.window, orient=tk.VERTICAL)
+        vbar.pack(side=tk.LEFT, fill=tk.Y)
+        vbar.config(command=canvas.yview)
+        # set scroll config for canvas
+        canvas.config(xscrollcommand=hbar.set, yscrollcommand= vbar.set)
         canvas.pack(expand=True, fill=tk.BOTH)
+        # mid position will be used to generate circle positions. 
         self.mid_position = (self.frame_width/2 ,self.frame_height/2)
+        #generate linked list then draw all nodes
         self.head = generate_linked_list(n)
         self.drawNode(self.head)
         current = self.head.next
         while current != self.head:
             self.drawNode(current)
             current = current.next
+        # change the current item color to blue
         canvas.itemconfig(self.head.data, fill="blue")
 
     def drawNode(self, node: Node):
@@ -99,3 +137,17 @@ class GUI:
 
     def getCircleId(self, number: int) -> int:
         return 2*number-1
+
+    def showSurvivor(self):
+        messagebox.showinfo("Survivor", f"{self.head.data} survives!")
+        self.close_window()
+        self.button["state"] = "normal"
+
+    def showInvalidInputDialog(self, input):
+        messagebox.showerror('Input Error', f'Error: {input} is not a valid integer')
+
+    def showInputTooLargeDialog(self, input):
+        messagebox.showerror('Input Error', f'Error: {input} is too large!')
+    
+    def showInputTooSmallDialog(self, input):
+        messagebox.showerror('Input Error', f'Error: input "{input}" cannot be negative!')
